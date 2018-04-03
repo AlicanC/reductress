775428efb8452eba0b76524dc398b55a4bae3dad
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2018-04-04 00:41:35 +0300

    [reductress-redux] Fix mutator implementation

---
+++ packages/reductress-redux/src/createReduxStore.js
@@ -0,0 +1,29 @@
+// @flow
+
+import { createObservableProvider } from 'reductress';
+import { createStore as createReductressCoreStore } from 'reductress-core';
+
+import createReduxMutator, { type Reducer } from './createReduxMutator';
+import createReduxStoreInterface, { type ReduxStore } from './createReduxStoreInterface';
+
+export type Enhancer<State, Action> = (
+  createStore: typeof createReduxStore,
+) => (reducer: Reducer<State, Action>, initialState: State) => ReduxStore<State, Action>;
+
+export default function createReduxStore<State, Action>(
+  reducer: Reducer<State, Action>,
+  initialState: State,
+  enhancer: ?Enhancer<State, Action>,
+): ReduxStore<State, Action> {
+  if (enhancer) {
+    return enhancer(createReduxStore)(reducer, initialState);
+  }
+
+  const provider = createObservableProvider();
+  const reductressStore = createReductressCoreStore(provider, initialState);
+  const reduxMutator = createReduxMutator(reductressStore, reducer);
+
+  const reduxStoreInterface = createReduxStoreInterface(reductressStore, reduxMutator);
+
+  return reduxStoreInterface;
+}
