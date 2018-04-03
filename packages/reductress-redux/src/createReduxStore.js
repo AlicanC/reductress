6f9c3d0888ce8a629f2424d7a3ef8e2d2aa2240d
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2018-04-04 00:55:26 +0300

    [reductress-redux] Improve Action type

--- packages/reductress-redux/src/createReduxStore.js
+++ packages/reductress-redux/src/createReduxStore.js
@@ -3,14 +3,14 @@
 import { createObservableProvider } from 'reductress';
 import { createStore as createReductressCoreStore } from 'reductress-core';
 
-import createReduxMutator, { type Reducer } from './createReduxMutator';
+import createReduxMutator, { type ActionObject, type Reducer } from './createReduxMutator';
 import createReduxStoreInterface, { type ReduxStore } from './createReduxStoreInterface';
 
-export type Enhancer<State, Action> = (
+export type Enhancer<State, Action: ActionObject> = (
   createStore: typeof createReduxStore,
 ) => (reducer: Reducer<State, Action>, initialState: State) => ReduxStore<State, Action>;
 
-export default function createReduxStore<State, Action>(
+export default function createReduxStore<State, Action: ActionObject>(
   reducer: Reducer<State, Action>,
   initialState: State,
   enhancer: ?Enhancer<State, Action>,
