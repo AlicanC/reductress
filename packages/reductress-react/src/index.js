dd4a694c9926f5d06e8c17f674ae98e8ba44d98f
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2018-05-10 10:02:24 -0700

    WIP

--- packages/reductress-react/src/index.js
+++ packages/reductress-react/src/index.js
@@ -1,5 +1,8 @@
 // @flow
 
 import createContext from './createContext';
+import createProvider from './createProvider';
+import createConsumer from './createConsumer';
+import createConnect from './createConnect';
 
-export { createContext };
+export { createContext, createProvider, createConsumer, createConnect };
